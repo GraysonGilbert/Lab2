072c512b05be47ad0cce80aefb2bd5bb544b6c54
Grayson Gilbert <91498658+GraysonGilbert@users.noreply.github.com>
2021-09-27 20:24:51 +0000

    code

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-p = 25
-GPIO.setup(p, GPIO.OUT)
+p1 = 25
+p2 = 12
+p3 = 16
+I1 = 4
+I2 = 27
+GPIO.setup(p1, GPIO.OUT)
 
-pwm = GPIO.PWM(p, 100)
+pwm = GPIO.PWM(p1, 100)
 
 try:
   pwm.start(0)
   while 1:
     for dc in range(101):
-      pwm.ChangeDutyCycle(dc)
-      sleep(0.01)
+      if dc < 100:
+        pwm.ChangeDutyCycle(dc)
+        sleep(0.01)
+        if dc == 100:
+          dc = 0
+          for dc in range(101):
+           pwm.ChangeDutyCycle(100 - dc)
+           sleep(0.01) 
+
 except KeyboardInterrupt:
   print('\nExiting')
