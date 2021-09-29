c1a26b9759f2fbab90e951ed59445ad57b395e03
Grayson Gilbert <91498658+GraysonGilbert@users.noreply.github.com>
2021-09-29 17:27:47 +0000

    code

--- main.py
+++ main.py
@@ -10,41 +10,41 @@ I2 = 27
 GPIO.setup(p1, GPIO.OUT)
 GPIO.setup(p2, GPIO.OUT)
 GPIO.setup(p3, GPIO.OUT)
+
 GPIO.setup(I1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(I2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-GPIO.output(p1, 0)
-GPIO.output(p2, 0)
 
 in1 = GPIO.input(I1)
 in2 = GPIO.input(I2)
 
-pwm1 = GPIO.PWM(p1, 100)
-pwm2 = GPIO.PWM(p2, 100)
+#pwm1 = GPIO.PWM(p1, 100)
+#pwm2 = GPIO.PWM(p2, 100)
 
 def myCallback(pin):
 
   if pin == I1:
-      pwm1.start(0)
-
-      for dc in range(101):
-        pwm1.ChangeDutyCycle(dc)
-        sleep(0.01)
-        if dc == 100:
-          dc = 0
-          for dc in range(101):
-           pwm1.ChangeDutyCycle(100 - dc)
-           sleep(0.01) 
-  if pin == I2:
-    pwm2.start(0)
+    pwm = GPIO.PWM(p1, 100)
+    pwm.start(0)
 
     for dc in range(101):
-      pwm2.ChangeDutyCycle(dc)
+      pwm.ChangeDutyCycle(dc)
       sleep(0.01)
       if dc == 100:
         dc = 0
         for dc in range(101):
-         pwm2.ChangeDutyCycle(100 - dc)
+         pwm.ChangeDutyCycle(100 - dc)
+         sleep(0.01) 
+  if pin == I2:
+    pwm = GPIO.PWM(p2, 100)
+    pwm.start(0)
+
+    for dc in range(101):
+      pwm.ChangeDutyCycle(dc)
+      sleep(0.01)
+      if dc == 100:
+        dc = 0
+        for dc in range(101):
+         pwm.ChangeDutyCycle(100 - dc)
          sleep(0.01) 
 
 
